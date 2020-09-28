cbe54864b0f751b62bb504e386875a6b44237de3
Andres Mejias <aemabit@gmail.com>
2020-09-28 01:00:46 -0400

    contact page

---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss'],
+})
+export class ContactComponent implements OnInit {
+  public contactForm: FormGroup;
+
+  // tslint:disable-next-line: max-line-length
+  private emailPattern: any = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  constructor() {
+    this.contactForm = this.createForm();
+  }
+
+  ngOnInit(): void {}
+
+  get name() { return this.contactForm.get('name'); }
+  get email() { return this.contactForm.get('email'); }
+  get message() { return this.contactForm.get('message'); }
+
+  createForm() {
+    return new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.pattern(this.emailPattern)
+      ]),
+      name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      message: new FormControl('', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(120),
+      ]),
+    });
+  }
+
+  onResetForm(): void {
+    this.contactForm.reset();
+  }
+
+  onSaveForm(): void {
+    if (this.contactForm.valid) {
+      this.onResetForm();
+    }
+  }
+}
